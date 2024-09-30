895640b4ba9bcd345f1022538f4866acd6fd459f
LiptakReka <liptakr@kkszki.hu>
2024-09-30 10:10:43 +0200

    Nuget extensions

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f58455154012ffc71050cd3ac09c5654b5861e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e88700224b1fe2a1ae46c91060b4b5b0a3d28c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
